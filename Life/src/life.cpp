b564d53b558ea7bc157942a3f0e382018bc35faa
WenboKou <kouwenbocn@163.com>
2022-04-23 08:57:45 +0800

    passed mycolont.txt

--- Life/src/life.cpp
+++ Life/src/life.cpp
@@ -27,13 +27,83 @@ void printGrid(Grid<char>& matrix){
     }
 }
 
+int detectSurround(Grid<char>& matrix, int r, int c, int row, int column) {
+    // situation 1: A location that has zero or one neighbors will be empty in the next generation. If a cell was there, it dies.
+    int numX = 0;
+    if(c-1 > 0) {
+        if(matrix[r][c-1] == 'X') {
+            numX++;
+        }
+    }
+    if(c+1 < column) {
+        if(matrix[r][c+1] == 'X') {
+            numX++;
+        }
+    }
+
+    if(r-1 > 0) {
+        if(matrix[r-1][c] == 'X') {
+            numX++;
+        }
+        if(c-1 > 0) {
+            if(matrix[r-1][c-1] == 'X') {
+                numX++;
+            }
+        }
+        if(c+1 < column) {
+            if(matrix[r-1][c+1] == 'X') {
+                numX++;
+            }
+        }
+    }
+    if(r+1 < row) {
+        if(matrix[r+1][c] == 'X') {
+            numX++;
+        }
+        if(c-1 > 0){
+            if(matrix[r+1][c-1] == 'X') {
+                numX++;
+            }
+        }
+        if(c+1 < column) {
+            if(matrix[r+1][c+1] == 'X') {
+                numX++;
+            }
+        }
+    }
+    return numX;
+}
+
+Grid<char> generation(Grid<char>& matrix, int row, int column) {
+    int neighbors;
+    Grid<char> newmatrix(row, column);
+    for (int r = 0; r < matrix.numRows(); r++) {
+        for (int c = 0; c < matrix.numCols(); c++) {
+            neighbors = detectSurround(matrix, r, c, row, column);
+            //cout << "r, c, neighbors: " << r << c << neighbors << endl;
+            if (neighbors <= 1) {
+                newmatrix[r][c] = '-';
+            } else if (neighbors == 3) {
+                newmatrix[r][c] = 'X';
+            } else if (neighbors >= 4) {
+                newmatrix[r][c] = '-';
+            } else {
+                newmatrix[r][c] = matrix[r][c];
+            }
+        }
+    }
+    return newmatrix;
+}
+
 int main() {
     // TODO: Finish the program!
     ifstream stream;
     string line;
+    string mode;
+    string frames;
     int c;
 
-    string filename = promptUserForFile(stream, "Grid file input?");
+    string filename = promptUserForFile(stream, "Grid file name?");
     getline(stream, line);
     int row = stringToInteger(line);
     cout << "rows: " << row << endl;
@@ -41,18 +111,39 @@ int main() {
     int column = stringToInteger(line);
     cout << "columns: " << column << endl;
     Grid<char> matrix(row, column);
-    for (int r = 0; r < row; r++){
+    for (int r = 0; r < row; r++) {
         getline(stream, line);
         c = 0;
-        for(char symbol : line) {
+        for (char symbol : line) {
             matrix[r][c] = symbol;
             c++;
         }
     }
-    //cout << "matrix: " << matrix;
     stream.close();
-    cout << "Have a nice Life!" << endl;
     printGrid(matrix);
+    while (1) {
+        mode = getLine("a)nimate, t)ick, q)uit?");
+        mode = toLowerCase(mode);
+        if (mode == "a") {
+           frames = getLine("How many frames?");
+           for (int i = 0; i < stringToInteger(frames); i++) {
+               // one generation
+               matrix = generation(matrix, row, column);
+               clearConsole();
+               printGrid(matrix);
+               pause(50);
+           }
+        } else if (mode == "t") {
+            // one generation
+            matrix = generation(matrix, row, column);
+            printGrid(matrix);
+        } else if (mode == "q") {
+            // quit the game
+            break;
+        }
+    }
+    cout << "Have a nice Life!" << endl;
+
     return 0;
 }
 
